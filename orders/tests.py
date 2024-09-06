2d452dfdf2771322244e1ff0fbcdd0f751b31f65
juanpcabana <juanpx99@gmail.com>
2024-09-07 00:47:35 +0200

    pruebas unitarias

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,159 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from orders.models import Order
+from products.models import Product
+
+
+class OrderTests(APITestCase):
+    def setUp(self):
+        self.base_url = "/api/v1/orders/"
+        self.products_url = "/api/v1/products/"
+
+        def create_products():
+
+            data = {
+                "reference": "ART001",
+                "name": "New Product",
+                "description": "A new product",
+                "base_price": 150.00,
+                "tax_rate": 18.00,
+            }
+            response = self.client.post(self.products_url, data, format="json")
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+            self.assertEqual(Product.objects.count(), 1)
+            self.assertEqual(
+                Product.objects.get(reference="ART001").name, "New Product"
+            )
+
+            data = {
+                "reference": "ART002",
+                "name": "New new Product",
+                "description": "A new new product",
+                "base_price": 200.00,
+                "tax_rate": 25.00,
+            }
+            response = self.client.post(self.products_url, data, format="json")
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+            self.assertEqual(Product.objects.count(), 2)
+            self.assertEqual(
+                Product.objects.get(reference="ART001").name, "New Product"
+            )
+
+        self.create_products = create_products
+
+    def test_list_orders(self):
+        """
+        Ensure we can list orders.
+        """
+        self.create_products()
+
+        data = {
+            "items": [
+                {"product_id": 1, "quantity": 2},
+                {"product_id": 2, "quantity": 1},
+            ]
+        }
+
+        response = self.client.post(self.base_url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response_data = response.data["body"]
+        self.assertEqual(response_data["id"], 1)
+        self.assertEqual(len(response_data["items"]), 2)
+        self.assertEqual(response_data["items"][0]["product"]["reference"], "ART001")
+        self.assertEqual(response_data["items"][0]["product"]["name"], "New Product")
+        self.assertEqual(response_data["items"][0]["quantity"], 2)
+        self.assertEqual(response_data["items"][1]["product"]["reference"], "ART002")
+        self.assertEqual(
+            response_data["items"][1]["product"]["name"], "New new Product"
+        )
+        self.assertEqual(response_data["items"][1]["quantity"], 1)
+
+    def test_create_order(self):
+        """
+        Ensure we can create a new order object.
+        """
+        self.create_products()
+
+        data = {
+            "items": [
+                {"product_id": 1, "quantity": 2},
+                {"product_id": 2, "quantity": 1},
+            ]
+        }
+
+        response = self.client.post(self.base_url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Order.objects.count(), 1)
+        self.assertEqual(Order.objects.get(id=1).items.count(), 2)
+
+    def test_get_order(self):
+        """
+        Ensure we can get an order object.
+        """
+
+        self.create_products()
+
+        data = {
+            "items": [
+                {"product_id": 1, "quantity": 2},
+                {"product_id": 2, "quantity": 1},
+            ]
+        }
+
+        response = self.client.post(self.base_url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        order_data = response.data["body"]
+        order_id = order_data["id"]
+        url = self.base_url + f"{order_id}/"
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_data = response.data["body"]
+        self.assertEqual(response_data["id"], 1)
+        self.assertEqual(len(response_data["items"]), 2)
+        self.assertEqual(response_data["items"][0]["product"]["reference"], "ART001")
+        self.assertEqual(response_data["items"][0]["product"]["name"], "New Product")
+        self.assertEqual(response_data["items"][0]["quantity"], 2)
+        self.assertEqual(response_data["items"][1]["product"]["reference"], "ART002")
+        self.assertEqual(
+            response_data["items"][1]["product"]["name"], "New new Product"
+        )
+        self.assertEqual(response_data["items"][1]["quantity"], 1)
+
+    def test_update_order(self):
+        """
+        Ensure we can update an order object.
+        """
+        self.create_products()
+
+        data = {
+            "items": [
+                {"product_id": 1, "quantity": 2},
+                {"product_id": 2, "quantity": 1},
+            ]
+        }
+
+        response = self.client.post(self.base_url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        order_data = response.data["body"]
+        order_id = order_data["id"]
+        url = self.base_url + f"{order_id}/"
+        data = {
+            "items": [
+                {"product_id": 1, "quantity": 3},
+                {"product_id": 2, "quantity": 2},
+            ]
+        }
+        response = self.client.put(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_data = response.data["body"]
+        self.assertEqual(response_data["id"], 1)
+        self.assertEqual(len(response_data["items"]), 2)
+        self.assertEqual(response_data["items"][0]["product"]["reference"], "ART001")
+        self.assertEqual(response_data["items"][0]["product"]["name"], "New Product")
+        self.assertEqual(response_data["items"][0]["quantity"], 3)
+        self.assertEqual(response_data["items"][1]["product"]["reference"], "ART002")
+        self.assertEqual(
+            response_data["items"][1]["product"]["name"], "New new Product"
+        )
+        self.assertEqual(response_data["items"][1]["quantity"], 2)
--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,108 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+from products.models import Product
+
+
+class ProductTests(APITestCase):
+    def setUp(self):
+        self.base_url = "/api/v1/products/"
+
+    def test_list_products(self):
+        """
+        Ensure we can list products.
+        """
+        url = self.base_url
+        data = {
+            "reference": "123456",
+            "name": "Product 1",
+            "description": "This is a product",
+            "base_price": 100.0,
+            "tax_rate": 0.21,
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, 201)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        response_data = response.data["body"]
+        self.assertEqual(len(response_data), 1)
+        self.assertEqual(response_data[0]["reference"], "123456")
+        self.assertEqual(response_data[0]["name"], "Product 1")
+        self.assertEqual(float(response_data[0]["base_price"]), 100.0)
+        self.assertEqual(float(response_data[0]["tax_rate"]), 0.21)
+
+    def test_create_product(self):
+        """
+        Ensure we can create a new product object.
+        """
+
+        url = self.base_url
+        data = {
+            "reference": "ART002",
+            "name": "New Product",
+            "description": "A new product",
+            "base_price": 150.00,
+            "tax_rate": 18.00,
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Product.objects.count(), 1)
+        self.assertEqual(Product.objects.get(reference="ART002").name, "New Product")
+
+    def test_get_product(self):
+        """
+        Ensure we can get a product object.
+        """
+        url = self.base_url
+        data = {
+            "reference": "12345",
+            "name": "closet",
+            "description": "Buen closet",
+            "base_price": 1200.00,
+            "tax_rate": 21.00,
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, 201)
+        product_data = response.data["body"]
+        product_id = product_data["id"]
+        url = self.base_url + f"{product_id}/"
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(product_data["reference"], "12345")
+        self.assertEqual(product_data["name"], "closet")
+        self.assertEqual(product_data["description"], "Buen closet")
+        self.assertEqual(float(product_data["base_price"]), 1200)
+        self.assertEqual(float(product_data["tax_rate"]), 21.00)
+
+    def test_update_product(self):
+        """
+        Ensure we can update a product object.
+        """
+        url = self.base_url
+        data = {
+            "reference": "123456",
+            "name": "Product 1",
+            "description": "This is a product",
+            "base_price": 100.0,
+            "tax_rate": 0.21,
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, 201)
+        response_data = response.data["body"]
+        product_id = response_data["id"]
+        url = self.base_url + f"{product_id}/"
+        data = {
+            "reference": "123456",
+            "name": "Product 1",
+            "description": "This is a product",
+            "base_price": 200.0,
+            "tax_rate": 0.21,
+        }
+        response = self.client.put(url, data, format="json")
+        response_data = response.data["body"]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_data["reference"], "123456")
+        self.assertEqual(response_data["name"], "Product 1")
+        self.assertEqual(response_data["description"], "This is a product")
+        self.assertEqual(float(response_data["base_price"]), 200.0)
+        self.assertEqual(float(response_data["tax_rate"]), 0.21)
